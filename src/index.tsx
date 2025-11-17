5d86a296f3f667c3fe8a13bd7d7281f74c9c10d7
cosmiclatte <129236813+coslatte@users.noreply.github.com>
2025-11-17 11:50:25 -0500

    Add advancements

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,74 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, Detail } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
+import { title } from "process";
 
 type Values = {
   textfield: string;
-  textarea: string;
 };
 
-export default function Command() {
-  function handleSubmit(values: Values) {
-    console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
-  }
+const MARKDOWN_TEXT = "**YEA**, idk";
 
+export function DetailWindow() {
   return (
-    <Form
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} />
-        </ActionPanel>
+    <Detail
+      markdown={MARKDOWN_TEXT}
+      navigationTitle="Test"
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label title="Height" text={`1' 04"`} />
+          <Detail.Metadata.Label title="Weight" text="13.2 lbs" />
+          <Detail.Metadata.TagList title="Type">
+            <Detail.Metadata.TagList.Item text="Electric" color={"#eed535"} />
+          </Detail.Metadata.TagList>
+          <Detail.Metadata.Separator />
+          <Detail.Metadata.Link title="Evolution" target="https://www.pokemon.com/us/pokedex/pikachu" text="Raichu" />
+        </Detail.Metadata>
       }
-    >
-      <Form.TextField id="textfield" title="Find chord" placeholder="Enter chord" defaultValue="C major" />
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextArea id="input" info="Insert text" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-    </Form>
+    />
+  );
+}
+
+function buildMarkdown(value: string) {
+  return `**${value}**`;
+}
+
+function isChordLike(value: string): boolean {
+  return value === "Cmaj";
+}
+
+async function translate(value: string): Promise<boolean> {
+  // implement with Tonal.js
+  return new Promise((resolve) => {
+    const valid = isChordLike(value);
+    if (valid) {
+      const md = buildMarkdown(value);
+      console.log(md);
+    }
+    resolve(valid)
+  });
+}
+
+async function handleSubmit(values: Values) {
+  const isValid = await translate(values.textfield);
+  if (isValid) {
+    showToast({ title: "Submitted form", message: "See logs for submitted values." });
+  } else {
+    showFailureToast({ title: "Failed to translate", message: "It's not a valid chord." });
+  }
+}
+
+export default function Command() {
+  return (
+    <>
+      <Form
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm onSubmit={handleSubmit} />
+          </ActionPanel>
+        }
+      >
+        <Form.TextField id="textfield" title="Find chord" placeholder="Enter chord" defaultValue="C major" />
+      </Form>
+    </>
   );
 }
